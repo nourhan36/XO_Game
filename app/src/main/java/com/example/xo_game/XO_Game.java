25008fe4142b7be81537d432619d3c5ec71f546a
nourhan36 <nourhanibrahim2004@gmail.com>
2023-12-27 13:27:27 +0200

    xml created

---
+++ app/src/main/java/com/example/xo_game/XO_Game.java
@@ -0,0 +1,21 @@
+package com.example.xo_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class XO_Game extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xo_game);
+
+    }
+
+
+}
